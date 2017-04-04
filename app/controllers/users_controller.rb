8ed6781ee629b25be676f5a3cae0dd8312f9ca12
Molly <mstoople@gmail.com>
2017-04-04 16:59:10 -0400

    Created new user sign up form. Created new and create actions and private method for requiring and whitelisting parameters on user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new
+    @user.email = user_params[:email]
+    @user.password = user_params[:password]
+    @user.password_confirmation = user_params[:password_confirmation]
+
+    if @user.save
+      redirect to root_path
+    else
+      render "new"
+    end
+  end
+
+
+  private def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
