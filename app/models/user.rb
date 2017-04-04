c0008aaa6ca6d519ef57e8ccf2285f702bd1899c
Molly <mstoople@gmail.com>
2017-04-04 17:05:01 -0400

    Added BCrypt to gemfile, rebundled, added basic validations to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :password, confirmation: true
+  validates :password_confrimation, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
 end
