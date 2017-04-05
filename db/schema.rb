b9d315ccc9c1f93a02f6a91c3d292b4e10a4710a
Robby Dore <Rdore88@gmail.com>
2017-04-05 16:57:57 -0400

    Added paid column to model order.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405202807) do
-
+ActiveRecord::Schema.define(version: 20170405205648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170405202807) do
     t.datetime "delivered_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "paid"
   end
 
   create_table "products", force: :cascade do |t|
