6eb83a2a9ccfb36f5330f1e0dfb3a90c5d2273b0
Robby Dore <Rdore88@gmail.com>
2017-04-05 13:29:18 -0400

    Fixed create method on order controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
   def create
     @client = current_user
-    @line_items = @client.order.last.line_items
-    @subtotal = @line_items.calculate_subtotal
-    @tax = line_items.tax
+    @order = @client.order.last
+    @line_items = @order.line_items
+    @subtotal = @order.calculate_subtotal(@line_items)
+    @tax = @order.tax(@subtotal)
     @total = @subtotal + @tax
-    @order = @client.order.build(subtotal: @subtotal, tax: @tax, total: @total)
+    @order.update(subtotal: @subtotal, tax: @tax, total: @total)
   end
 end
