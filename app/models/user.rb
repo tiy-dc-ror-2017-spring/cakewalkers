f11c92544badc558d7e1e591f9ad56c5b8ff47b6
Molly <mstoople@gmail.com>
2017-04-05 16:05:48 -0400

    Removed unnecessary methods from User.rb that are controlled by has_secure_password internally.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,4 @@ class User < ApplicationRecord
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-
-  def password
-    @password ||= BCrypt::Password.new(password_digest)
-  end
-
-  def password=(plaintext_password)
-    @password = BCrypt::Password.create(plaintext_password)
-    self.password_digest = @password
-  end
 end
