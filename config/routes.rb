e2f8a77904a6bfad3813bdda17529e9d7ec7fc41
Molly <mstoople@gmail.com>
2017-04-05 16:29:43 -0400

    Created sessions controller and new view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'new/create'
+
+  get 'new/destroy'
+
   resource :users, only: [:new, :create]
   resources :products
 end
