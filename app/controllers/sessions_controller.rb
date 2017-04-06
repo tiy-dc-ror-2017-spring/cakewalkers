959ec37efbb2f8186788e4e330b2a1c33f58e689
Molly <mstoople@gmail.com>
2017-04-06 14:06:16 -0400

    Created login and logout links in layouts.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
 
-    if @user && @user.password == params[:password]
+    if @user && @user.authenticate(params[:password])
       session[:current_user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to new_sessions_path
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session[:current_user_id] = nil
-    redirect_to new_sessions_path
+    redirect_to new_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :products
 
   resource :users, only: [:new, :create]
+
+  root controller: "products", action: "index" # TODO: change to a homepage of some kind
 end
