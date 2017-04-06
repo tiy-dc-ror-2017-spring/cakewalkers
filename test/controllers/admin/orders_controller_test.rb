7afe9567bb4c45fa6b953f162e4310fea317200a
Russell Osborne <russell@burningpony.com>
2017-04-06 19:49:18 -0400

    Fixed broken generated tests from in class examples.

--- test/controllers/admin/orders_controller_test.rb
+++ test/controllers/admin/orders_controller_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class Admin::OrdersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get admin_orders_index_url
+    get admin_orders_url
     assert_response :success
   end
 
   test "should get show" do
-    get admin_orders_show_url
+    get admin_order_url(Order.create)
     assert_response :success
   end
-
 end
