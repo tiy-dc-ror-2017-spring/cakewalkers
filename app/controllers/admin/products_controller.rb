f394831fd4514b197ca57a9b32a13e3b3b54f248
Robby Dore <Rdore88@gmail.com>
2017-04-06 17:09:19 -0400

    Fixed admin products.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,25 +1,20 @@
 class Admin::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /products
   def index
     @products = Product.all
   end
 
-  # GET /products/1
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
   def create
     @product = Product.new(product_params)
 
@@ -30,7 +25,6 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1
   def update
     if @product.update(product_params)
       redirect_to admin_products_path, notice: 'Product was successfully updated.'
@@ -39,7 +33,6 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1
   def destroy
     @product.destroy
     redirect_to admin_products_path, notice: 'Product was successfully destroyed.'
