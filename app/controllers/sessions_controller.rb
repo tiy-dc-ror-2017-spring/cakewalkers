fcad50298f8039ee9739cd867511dcceb9737b4c
Molly <mstoople@gmail.com>
2017-04-06 13:22:24 -0400

    Generated actions for sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.password == params[:password]
+      session[:current_user_id] = @user.id
+    else
+      redirect_to new_sessions_path
+    end
   end
 
   def destroy
+    session[:current_user_id] = nil
+    redirect_to new_sessions_path
   end
 end
