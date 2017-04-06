6bb8a4faa9031e8714e63c264aaff3fd70efe18f
Molly <mstoople@gmail.com>
2017-04-06 13:19:11 -0400

    Generated sessions controllerwith new create and destroy.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user.password = user_params[:password]
     @user.password_confirmation = user_params[:password_confirmation]
 
-    @user.save #TODO remove and institute commented out 
+    @user.save #TODO remove and institute commented out
     # # commented out pending routes actually being generated...
     # if @user.save
     #   redirect to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :orders, only: [:create]
 
   resources :products
