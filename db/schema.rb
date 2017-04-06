0d5ce76c6c964bb139ff794b52bb17436075d365
James Webb <webbfamily@Jamess-MBP.lan>
2017-04-06 15:03:44 -0400

    stage schema to get up to date

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20170405202807) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "line_items", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "quantity"
-    t.integer  "order_id"
-    t.integer  "line_item_total"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "client_id"
