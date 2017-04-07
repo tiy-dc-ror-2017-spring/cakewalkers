c2d0e4c0b0c8152f77a49845f3272f231eac4986
James Webb Jr <jamespkiddo@yahoo.com>
2017-04-07 15:36:01 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -67,14 +67,6 @@ ActiveRecord::Schema.define(version: 20170407005131) do
     t.index ["user_id"], name: "index_staff_members_on_user_id", using: :btree
   end
 
-  create_table "staffs", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_staffs_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
