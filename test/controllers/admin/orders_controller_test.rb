36bb164379db3ae5a1f18b85775e6fb53382520d
Russell Osborne <russell@burningpony.com>
2017-04-06 20:17:05 -0400

    Test now actually verifies that last order is on page.

--- test/controllers/admin/orders_controller_test.rb
+++ test/controllers/admin/orders_controller_test.rb
@@ -11,10 +11,10 @@ class Admin::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index second page" do
-    4.times { Order.create }
+    4.times { |i| Order.create(total: i * 100) }
     get admin_orders_url, params: { page: 2, per_page: 2 }
     assert_response :success
-    assert_select "td", number_to_currency(Order.last.total)
+    assert_select "tr>td", text: number_to_currency(Order.last.total)
   end
 
   test "should get show" do
