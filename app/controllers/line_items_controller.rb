cfb5bb07ed0104c799ffb12f44819d2fc17984a6
Robby Dore <Rdore88@gmail.com>
2017-04-07 11:36:46 -0400

    Initial set up

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,44 @@
 class LineItemsController < ApplicationController
+  before_action :authorize!
+  def new
+    if user_last_order.paid == true
+      @order = current_user.order.create
+      @line_item = @order.line_item.new
+    else
+      @line_item = user_last_order.line_item.new
+    end
+  end
+
+  def create
+    @line_item = user_last_order.line_item.build(line_item_params)
+  end
+
+  def edit
+    @line_item = LineItem.find(params[:id])
+  end
+
+  def update
+    @line_item = LineItem.find(params[:id])
+    @line_item.update(line_item_params)
+    @line_item.save
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:quantity, :product_id)
+  end
+
+  def user_last_order
+    if current_user.order
+      current_user.order.last
+    end
+  end
+
+  def authorize!
+    unless current_user.clients
+      session[:error] = "Please log in before you order"
+      redirect_to products_path
+    end
+  end
 end
