ac094518182e1e8be087b2320c165c10ea787d33
Russell Osborne <russell@burningpony.com>
2017-04-06 20:34:12 -0400

    Refactored and commented admin order pagination.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,21 +1,23 @@
 class Admin::OrdersController < ApplicationController
   def index
     @page = (params[:page] || 1).to_i
-    if @page.negative? || @page == 0
-      @page = 1
-    end
 
+    # If an invalid page number, go to the first page.
+    @page = @page <= 0 ? 1 : @page
     @per_page = (params[:per_page] || 100).to_i
+
     @total_pages = (Order.count.to_f / @per_page).ceil
+    # If there are no pages there is always 1 page.
     @total_pages = @total_pages > 0 ? @total_pages : 1
 
-    if @page > @total_pages
-      @page = @total_pages
-    end
+    # If a request page is greater than max, default to last page.
+    @page = @page > @total_pages ? @total_pages : @page
 
-    @orders = Order.order(created_at: :desc).limit(@per_page).offset(@per_page * (@page - 1) )
+    @orders = Order \
+              .order(created_at: :desc)
+              .limit(@per_page)
+              .offset(@per_page * (@page - 1))
   end
 
-  def show
-  end
+  def show; end
 end
