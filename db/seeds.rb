75b59aaadd502e90f4dfc0526ff4c9a412cdb1ef
James Webb Jr <jamespkiddo@yahoo.com>
2017-04-07 15:40:23 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 staff_members = StaffMember.create!([
   { name: "Molly", user: User.create(email: "molly@stoopler.com", password_digest: "password") },
   { name: "James", user: User.create(email: "james@webb.com", password_digest: "password") },
