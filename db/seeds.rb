3e421e6df4b06006f192f62f167a43f3c9b32fdd
Molly <mstoople@gmail.com>
2017-04-09 23:25:49 -0400

    Added icon, changed color-scheme, seeded database. Fixed issue where page would not load if product image was not present.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([{ name: "Spice Cake", unit_price_in_cents: 2200,}, { name: "Pullman Loaf", unit_price_in_cents: 800 }, { name: "Muffin", unit_price_in_cents: 300 }, { name: "Avocado Cake", unit_price_in_cents: 3800 }, {
+  name: "Whoopie Pie", unit_price_in_cents: 300 }])
