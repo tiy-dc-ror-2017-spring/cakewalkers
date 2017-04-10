c2cea3d5ee42c976dd55de802bfac149fa970de5
Robby Dore <Rdore88@gmail.com>
2017-04-10 15:21:44 -0400

    Seeded products, changed views so they won't crash.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 products = HTTParty.get("https://cakewalkers-api.herokuapp.com/products")
-Product.create([products])
+products.each do |product_attrs|
+  product = Product.new
+  product.code = product_attrs["code"]
+  product.name = product_attrs["name"]
+  product.save
+end
 
-staff_members = StaffMember.create!([
-  { name: "Molly", user: User.create(email: "molly@stoopler.com", password: "password", password_confirmation: "password") },
-  { name: "James", user: User.create(email: "james@webb.com", password: "password", password_confirmation: "password") },
-  { name: "Robby", user: User.create(email: "robby@dore.com", password: "password", password_confirmation: "password")},
-  { name: "Russell", user: User.create(email: "russell@osborne.com", password: "password", password_confirmation: "password") }
-]
-)
+# staff_members = StaffMember.create!([
+#   { name: "Molly", user: User.create(email: "molly@stoopler.com", password: "password", password_confirmation: "password") },
+#   { name: "James", user: User.create(email: "james@webb.com", password: "password", password_confirmation: "password") },
+#   { name: "Robby", user: User.create(email: "robby@dore.com", password: "password", password_confirmation: "password")},
+#   { name: "Russell", user: User.create(email: "russell@osborne.com", password: "password", password_confirmation: "password") }
+# ]
+# )
